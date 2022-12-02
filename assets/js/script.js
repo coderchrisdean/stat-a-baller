98be0f70dbbdab4954259425f5fea562bd6e0367
mengxue xu <mxu4321@gmail.com>
2022-12-01 16:15:07 -0800

    added 2 more html & display content ideas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
 
+// fetch('https://www.balldontlie.io/api/v1/players/237')
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
 
-fetch('https://www.balldontlie.io/api/v1/players/237')
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+//     fetch('https://www.balldontlie.io/api/v1/season_averages?player_ids[1-500]')
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
 
-    fetch('https://www.balldontlie.io/api/v1/season_averages?player_ids[1-500]')
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
 
-    
+// ----------- home page: hide card -----------
+document.querySelector("#lebron-james").style.display = "none";
