13f6d999e45daf65a4cb59a83bab38b7ad5a5a0b
mengxue xu <mxu4321@gmail.com>
2022-12-05 20:46:15 -0800

    updated html js file

---
+++ assets/js/option_player.js
@@ -0,0 +1,6 @@
+// ----------- ⬇ mx: divider ⬇ -----------
+// ----------- mx: get user searched player from local storage -----------
+var userSearchPlayer = JSON.parse(localStorage.getItem("userSearchPlayer"));
+// get stats and display on option_player_page.html
+
+// ----------- ⬆ mx: divider ⬆ -----------
