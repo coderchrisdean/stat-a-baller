e510fc4f9fb2d82905b8e46fdab139194f2a2b44
coderchrisdean <coderchrisdean@gmail.com>
2022-12-07 13:15:45 -0800

    removed commented code on buildhtml.js

--- assets/js/buildhtml.js
+++ assets/js/buildhtml.js
@@ -11,19 +11,3 @@ function show() {
 }
 
 
-// const show = document.getElementById("element-id").classList.add("is-hidden");
-
-// {
-
-// hideSearchButton ()
-// {
-//     var searchButton = document.getElementById("searchBtn");
-//     searchButton.classList.add("is-hidden");
-// }
-
-//   showSearchButton ()
-//   {
-//     var searchForPlayerElement = document.getElementById("searchForPlayer");
-//     searchForPlayerElement.classList.remove(" is-hidden");
-//   }
-// }
