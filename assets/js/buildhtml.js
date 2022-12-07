3c08fc39d5606f263069c361767ec3bb64190a59
coderchrisdean <coderchrisdean@gmail.com>
2022-12-07 12:41:06 -0800

    changed buildhtml.js, and frame.html

--- assets/js/buildhtml.js
+++ assets/js/buildhtml.js
@@ -1,24 +1,29 @@
-const hide = function () {
-    document.getElementById("element-id").classList.remove("is-hidden");
+// finds the searchForPlayer section
+var searchForPlayerEl = document.getElementById("searchForPlayer");
+var btnGetStarted = document.getElementById("btnGetStarted");
+
+var showSearch = btnGetStarted.addEventListener("click", show);
+
+function show() {
+  searchForPlayerEl.classList.remove("section is-hidden");
+  searchForPlayerEl.classList.add("section");
+  console.log("success");
 }
 
-const show = document.getElementById("element-id").classList.add("is-hidden");
 
+// const show = document.getElementById("element-id").classList.add("is-hidden");
 
-{
+// {
 
-hideSearchButton () 
-{
-    var searchButton = document.getElementById("searchBtn");
-    searchButton.classList.add("is-hidden");
-}
- 
-  
-  showSearchButton () 
-  {
-    var searchForPlayerElement = document.getElementById("searchForPlayer");
-    searchForPlayerElement.classList.remove(" is-hidden");
-  }
-}
-addEventListener();
-  
+// hideSearchButton ()
+// {
+//     var searchButton = document.getElementById("searchBtn");
+//     searchButton.classList.add("is-hidden");
+// }
+
+//   showSearchButton ()
+//   {
+//     var searchForPlayerElement = document.getElementById("searchForPlayer");
+//     searchForPlayerElement.classList.remove(" is-hidden");
+//   }
+// }
