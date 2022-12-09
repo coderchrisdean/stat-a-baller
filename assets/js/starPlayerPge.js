4d2803e26b01da5201b2e1146b299bfe36aa1f4a
mengxue xu <mxu4321@gmail.com>
2022-12-08 20:31:09 -0800

    updated js file srcs, js functions for buttons

--- assets/js/starPlayerPge.js
+++ assets/js/starPlayerPge.js
@@ -4,12 +4,16 @@ var position = document.getElementById("POSITION");
 var height = document.getElementById("HEIGHT");
 var team = document.getElementById("TEAM");
 
+// get player name by clicking on the button of players in the index.html
+
+
 var getPlayerId = function (playerName) {
     console.log(playerName);
     var playerStats = `https://www.balldontlie.io/api/v1/players?search=${playerName}`;
     fetch(playerStats)
         .then((response) => response.json())
             .then((data) => {
+                 console.log(data);
                 var playerID = data.data[0].id;
                 console.log(playerID);
                 // sends id to get the actual players stats
@@ -21,16 +25,20 @@ var getPlayerId = function (playerName) {
             }
         });
     }
-var getPlayerStats = (id) => {
-    let playerStats = `https://www.balldontlie.io/api/v1/players/${id}`;
+
+
+var getPlayerStats = (playerID) => {
+    let playerStats = `https://www.balldontlie.io/api/v1/players/${playerID}`;
+    // unable to get stats
     console.log(playerStats);
     // fetch player by id
     fetch(playerStats)
         .then((response) => response.json())
             .then((data) => {
+                // console.log(data);
                 // set html to our data
                 position.innerHTML = data.position;
-                console.log(position);
+                // console.log(position);
                 height.innerHTML = data.height_feet + "'" + data.height_inches;
                 team.innerHTML = data.team.full_name;
                 weight.innerHTML = data.weight_pounds;
